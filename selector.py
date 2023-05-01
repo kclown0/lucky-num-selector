2daf275b6b10fcda49458a7c38fffd71ccf8aa0b
kclown0 <keumdy0@gmail.com>
2023-05-01 14:30:43 +0900

    feat: Edit selectoy.py
    - 입력하는 뽑기 횟수만큼 1~45의 숫자 중 6개의 숫자 뽑기

--- selector.py
+++ selector.py
@@ -1,4 +1,8 @@
 import random
+print('원하는 뽑기횟수를 입력해주세요')
+s = int(input())
 
-numbers = random.sample(range(1, 46), 6)
-print(numbers)
+for i in range(s):
+    if i < s:
+        numbers = random.sample(range(1, 46), 6)
+        print(numbers)
